baa8c4612954bd34a33b0c203b1196f3f6059b07
Yauheni Sidarenka <Yauheni_Sidarenka@epam.com>
2014-09-04 11:56:44 +0300

    some cosmetic changes in calculator's Main

--- src/com/epam/Main.java
+++ src/com/epam/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Application run!");
         System.out.println("Divide 5/3:");
-        System.out.println("Double calc:" + new DoubleCalculator().divide(5.0, 3.0));
-        System.out.println("Float calc" + new FloatCalculator().divide(5.0, 3.0));
+        System.out.println("Double calc: " + new DoubleCalculator().divide(5.0, 3.0));
+        System.out.println("Float calc:  " + new FloatCalculator().divide(5.0, 3.0));
     }
 }
